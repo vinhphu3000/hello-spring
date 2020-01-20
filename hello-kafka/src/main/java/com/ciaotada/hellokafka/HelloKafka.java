5993116ff85a973bf93a9d91df1ae7a4b40a876f
Phu Ha <phu.ha@ciaotada,com>
2020-01-20 17:13:25 +0700

    init

---
+++ hello-kafka/src/main/java/com/ciaotada/hellokafka/HelloKafka.java
@@ -0,0 +1,7 @@
+package com.ciaotada.hellokafka;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloKafka {
+}
---
+++ hello-kafka/src/main/java/com/ciaotada/hellokafka/HelloKafkaApplication.java
@@ -0,0 +1,14 @@
+package com.ciaotada.hellokafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloKafkaApplication.class, args);
+	}
+
+
+}
